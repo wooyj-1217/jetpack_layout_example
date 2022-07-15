e48e57e8677a1486782bf837fbb053f3e6d1aeb8
wooyj-1217 <shamoo1217@gmail.com>
2022-07-15 10:39:29 +0900

    복잡한 맞춤 레이아웃 부분까지 그려봄.

--- app/src/main/java/com/wooyj/jetpackcomposetest/MainActivity.kt
+++ app/src/main/java/com/wooyj/jetpackcomposetest/MainActivity.kt
@@ -4,22 +4,33 @@ import android.os.Bundle
 import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DocumentScanner
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.AlignmentLine
+import androidx.compose.ui.layout.FirstBaseline
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layout
 import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberImagePainter
 import com.wooyj.jetpackcomposetest.ui.theme.JetpackComposeTestTheme
 import kotlinx.coroutines.launch
@@ -82,11 +93,31 @@ fun HeaderContent() {
 
 @Composable
 fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier.padding(8.dp)) {
-//        Text("column1")
-//        Text("column2")
-//        SimpleList()
-        LazyList()
+//    Column(modifier = modifier.padding(8.dp)) {
+//        Text("column")
+//        Text("column")
+//        Text("column")
+//        Text("column")
+//        Text("column")
+//        Text("column")
+////        SimpleList()
+////        LazyList()
+//    }
+//    MyOwnColumn(modifier = modifier.padding(8.dp)) {
+//        Text("MyOwnColumn")
+//        Text("MyOwnColumn")
+//        Text("MyOwnColumn")
+//        Text("MyOwnColumn")
+//        Text("MyOwnColumn")
+//        Text("MyOwnColumn")
+//
+//    }
+    Row(modifier = modifier.horizontalScroll(rememberScrollState())) {
+        StaggeredGrid(modifier = modifier, rows = 3) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic.first, count = topic.second)
+            }
+        }
     }
 }
 
@@ -156,3 +187,199 @@ fun ImageListItem(index: Int) {
 }
 
 
+// 레이아웃 수정자
+fun Modifier.firstBaselineToTop(
+    firstBaselineToTop: Dp
+) = this.then(
+    layout { measurable, constraints ->
+
+        val placeable = measurable.measure(constraints)
+
+        // 컴포서블이 첫번째 baseline을 가지고 있는지 확인
+        check(placeable[FirstBaseline] != AlignmentLine.Unspecified)
+        val firstBaseline = placeable[FirstBaseline]
+
+        // padding을 가지고 있는 컴포서블의 높이값 - 첫번째 baseline 값
+        val placeableY = firstBaselineToTop.roundToPx() - firstBaseline
+        val height = placeable.height + placeableY
+        layout(placeable.width, height) {
+            placeable.placeRelative(0, placeableY)
+        }
+    }
+)
+
+// BaselineHeight + paddingTop = 32dp
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    JetpackComposeTestTheme {
+        Text("테스트용", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+// paddingTop = 32dp
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    JetpackComposeTestTheme {
+        Text("테스트용", Modifier.padding(top = 32.dp))
+    }
+}
+
+
+// layout composable 사용하기
+@Composable
+fun MyOwnColumn(modifier: Modifier, content: @Composable () -> Unit) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            // Place children
+            placeables.forEach { placeable ->
+                placeable.placeRelative(0, yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+
+// 복잡한 맞춤 레이아웃 만들기
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+
+            placeable
+        }
+
+        // Grid's width is the widest row
+        val width = rowWidths.maxOrNull()
+            ?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+
+        // Grid's height is the sum of the tallest element of each row
+        // coerced to the height constraints
+        val height = rowHeights.sumOf { it }
+            .coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        // Y of each row, based on the height accumulation of previous rows
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i - 1] + rowHeights[i - 1]
+        }
+
+        // Set the size of the parent layout
+        layout(width, height) {
+            // x cord we have placed up to, per row
+            val rowX = IntArray(rows) { 0 }
+
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+                placeable.placeRelative(
+                    x = rowX[row],
+                    y = rowY[row]
+                )
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String, count: Int) {
+    Card(
+        modifier = modifier,
+        shape = RoundedCornerShape(0.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(60.dp, 60.dp)
+                    .background(color = MaterialTheme.colorScheme.secondary)
+            )
+            Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+                Text(
+                    text = text, style = TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Icon(
+                        imageVector = Icons.Filled.DocumentScanner,
+                        contentDescription = null,
+                        Modifier.size(8.dp),
+                        tint = Color.Gray
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        text = count.toString(), style = TextStyle(
+                            fontSize = 8.sp,
+                            fontWeight = FontWeight.Light,
+                            color = Color.Gray
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    JetpackComposeTestTheme {
+        Chip(text = "테스트", count = 8)
+    }
+}
+
+val topics = listOf(
+    Pair("Arts & Crafts", 121),
+    Pair("Beauty", 80),
+    Pair("Books", 42),
+    Pair("Business", 78),
+    Pair("Comics", 30),
+    Pair("Culinary", 118),
+    Pair("Design", 45),
+    Pair("Fashion", 92),
+    Pair("Film", 23),
+    Pair("History", 58),
+    Pair("Maths", 90),
+    Pair("Music", 222),
+    Pair("People", 231),
+    Pair("Philosophy", 20),
+    Pair("Religion", 24),
+    Pair("Social sciences", 37),
+    Pair("Technology", 403),
+    Pair("TV", 424),
+    Pair("Writing", 34),
+)
